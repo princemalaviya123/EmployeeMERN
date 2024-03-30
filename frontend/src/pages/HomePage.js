7ca8523cad93d591440db72431ad035c63179502
princemalaviya123 <princemalaviya16@gmail.com>
2024-03-31 00:01:31 +0530

    update name

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -67,7 +67,7 @@ const HomePage = () => {
           <Link to={"/addemployee"}>
             <div>
               <button className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-sm font-semibold leading-7 text-white hover:bg-indigo-500 ">
-                Add milan
+                Add Employee
               </button>
             </div>
           </Link>
